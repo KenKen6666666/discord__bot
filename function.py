783178d00145b172ffb1bd4e294097b194640db4
KenKen6666666 <Ken970116g@gmail.com>
2024-08-17 14:50:18 +0800

    Add files via upload

---
+++ function.py
@@ -0,0 +1,54 @@
+def isDateValid(date):
+    date_list = date.split("/")
+    try:
+        year = int(date_list[0])
+        month = int(date_list[1].lstrip("0"))
+        day = int(date_list[2].lstrip("0"))
+    except:
+        return 1
+    if len(date_list) != 3 or not((str(year) and str(month) and str(day)).isdigit()):
+        return 1
+    if len(date_list[1]) != 2 or len(date_list[2]) != 2:
+        return 1
+    else:
+        if month == 0 or month > 12:
+            return 2
+        else:
+            if month == 2:
+                if (year % 4 == 0 and year % 100 != 0) or (year % 4 == 0 and year % 100 == 0 and year % 400==0):
+                    if day == 0 or day > 29:
+                        return 3
+                    else:
+                        return 4
+                else:
+                    if day == 0 or day > 28:
+                        return 3
+                    else:
+                        return 4
+            elif month in [1,3,5,7,8,10,12]:
+                if day == 0 or day > 31:
+                    return 3
+                else:
+                    return 4
+            else:
+                if day == 0 or day > 30:
+                    return 3
+                else:
+                    return 4
+                
+def guess_word(guess, solution):
+    feedback = ["#"] * 5
+    solution_copy = list(solution)
+
+    for i in range(5):
+        if guess[i] == solution[i]:
+            feedback[i] = guess[i].upper()
+            solution_copy[i] = None
+
+    for i in range(5):
+        if feedback[i] == "#":
+            if guess[i] in solution_copy:
+                feedback[i] = guess[i].lower()
+                solution_copy[solution_copy.index(guess[i])] = None
+
+    return "".join(feedback)
